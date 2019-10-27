158ccd277a0c25ac0878b123a647b8c75f86cbe3
ShunOmine <shun12baseball@gmail.com>
2019-10-27 14:29:14 +0900

    create footer,header,progress components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { LoadingProgress, CircularIntegration } from "../lib/ui/LoadingProgress";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <h1>Hello React</h1>
+        <LoadingProgress />
+        <CircularIntegration />
       </React.Fragment>
     );
   }
